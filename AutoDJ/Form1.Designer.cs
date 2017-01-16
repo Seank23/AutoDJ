8e735c9023144a98d59f0a912325a55eebf6dbfb
SEAN-PC\seank <sean.king@gmx.com>
2017-01-16 20:04:53 +0000

    v0.0.1 - Working YouTube search & play

---
+++ AutoDJ/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace AutoDJ
+{
+    partial class frmAutoDJ
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtCriteria = new System.Windows.Forms.TextBox();
+            this.lblSong = new System.Windows.Forms.Label();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.lblName = new System.Windows.Forms.Label();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.txtDuration = new System.Windows.Forms.TextBox();
+            this.lblDuration = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtCriteria
+            // 
+            this.txtCriteria.Location = new System.Drawing.Point(110, 33);
+            this.txtCriteria.Name = "txtCriteria";
+            this.txtCriteria.Size = new System.Drawing.Size(156, 20);
+            this.txtCriteria.TabIndex = 0;
+            // 
+            // lblSong
+            // 
+            this.lblSong.AutoSize = true;
+            this.lblSong.Location = new System.Drawing.Point(69, 36);
+            this.lblSong.Name = "lblSong";
+            this.lblSong.Size = new System.Drawing.Size(35, 13);
+            this.lblSong.TabIndex = 1;
+            this.lblSong.Text = "Song:";
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(281, 31);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch.TabIndex = 2;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(110, 95);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(246, 20);
+            this.txtName.TabIndex = 3;
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(38, 98);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(66, 13);
+            this.lblName.TabIndex = 4;
+            this.lblName.Text = "Song Name:";
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(281, 61);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 5;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // txtDuration
+            // 
+            this.txtDuration.Location = new System.Drawing.Point(110, 122);
+            this.txtDuration.Name = "txtDuration";
+            this.txtDuration.Size = new System.Drawing.Size(100, 20);
+            this.txtDuration.TabIndex = 6;
+            // 
+            // lblDuration
+            // 
+            this.lblDuration.AutoSize = true;
+            this.lblDuration.Location = new System.Drawing.Point(26, 125);
+            this.lblDuration.Name = "lblDuration";
+            this.lblDuration.Size = new System.Drawing.Size(78, 13);
+            this.lblDuration.TabIndex = 7;
+            this.lblDuration.Text = "Song Duration:";
+            // 
+            // frmAutoDJ
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(405, 195);
+            this.Controls.Add(this.lblDuration);
+            this.Controls.Add(this.txtDuration);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.lblSong);
+            this.Controls.Add(this.txtCriteria);
+            this.Name = "frmAutoDJ";
+            this.Text = "AutoDJ";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtCriteria;
+        private System.Windows.Forms.Label lblSong;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.TextBox txtDuration;
+        private System.Windows.Forms.Label lblDuration;
+    }
+}
+
---
+++ AutoDJ/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Net;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AutoDJ
+{
+    public partial class frmAutoDJ : Form
+    {
+        public frmAutoDJ()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            string criteria = txtCriteria.Text;
+            criteria.Replace(' ', '+');
+            string searchURL = "http://www.youtube.com/results?search_query=" + criteria;
+            string html = GetHTML(searchURL);
+            string videoHTML = FindFromSource(html, "watch?", "div class", 2);
+            string videoURL = "http://www.youtube.com/" + FindFromSource(videoHTML, "watch?", '"'.ToString(), 1);
+            string songName = FindFromSource(videoHTML, "dir", '<'.ToString(), 1);
+            songName = songName.Substring(10, songName.Length - 10);
+            string songDuration = FindFromSource(videoHTML, "Duration: ", '.'.ToString(), 1);
+            songDuration = songDuration.Substring(10, songDuration.Length - 10);
+
+            txtName.Text = songName;
+            txtDuration.Text = songDuration;
+            System.Diagnostics.Process.Start(videoURL);
+        }
+
+        private string GetHTML(string url)
+        {
+            string html = "";
+
+            using (WebClient client = new WebClient())
+            {
+                html = client.DownloadString(url);
+            }
+
+            return html;
+        }
+
+        private string FindFromSource(string source, string startTerm, string endTerm, int occurence)
+        {
+            int start, end;
+            string text = "";
+
+            if(source.Contains(startTerm))
+            {
+                start = source.IndexOf(startTerm, 0);
+
+                while(occurence > 1)
+                {
+                    start = source.IndexOf(startTerm, start + 1);
+                    occurence--;
+                }
+
+                end = source.IndexOf(endTerm, start);
+                text = source.Substring(start, end - start);
+            }
+
+            return text;
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtCriteria.Clear();
+            txtName.Clear();
+            txtDuration.Clear();
+        }
+    }
+}
